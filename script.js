352227596b99e33ceec52df21a481da1ae330635
none use <noneuse.act@gmail.com>
2024-03-31 14:20:08 +0530

    js program

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// Student mark program
+const studentMark = 50;
+
+if (studentMark < 50) {
+    console.log(`your mark is ${studentMark} your failed`);
+}else{
+    console.log(`your mark is ${studentMark} your passed`);
+}
+
+// OTT platform program
+const yourAge = 21;
+
+if (yourAge >= 18) {
+    console.log("Your eligible enter the ott platform");
+}else {
+    console.log(`your age is below 18 then you not eligible entering`)
+}
+
+
+// squar root program
+const number = 16;
+
+let squarRoot = Math.sqrt(number)
+console.log(squarRoot);
